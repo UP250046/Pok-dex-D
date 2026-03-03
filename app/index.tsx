80884d2ea6e7a66dd518117da11655f50f64e6b6
David Avila <up250046@alumnos.upa.edu.mx>
2026-03-03 15:34:13 -0600

    add getPokemons fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    console.log("Ectre en pantalla");
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(response);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>David</Text>
     </View>
   );
 }
