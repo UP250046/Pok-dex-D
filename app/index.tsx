0227babd3f533f10a136b4d3e3a3b72f9390eae6
David Avila <up250046@alumnos.upa.edu.mx>
2026-03-04 20:02:09 -0600

    Show pokemos on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [results, setResults] = useState<any[]>([]);
+
   useEffect(() => {
-    console.log("Ectre en pantalla");
+    console.log("Entre en pantalla");
     getPokemons();
   }, []);
 
   const getPokemons = async () => {
-    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0";
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(response);
+    console.log(data);
+    setResults(data.results);
   };
 
   return (
     <View>
-      <Text>David</Text>
+      <Text>{results[1]?.name}</Text>
+      {results.map((item) => (
+        <Text key={item.name}>{item.name}</Text>
+      ))}
     </View>
   );
 }
